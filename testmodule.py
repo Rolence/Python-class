7230e73b1c66f0f018f9f9227a2f348378124c66
Achia Rolence <akarolence12@gmail.com>
2017-10-20 15:19:13 +0100

    testmodule.py

---
+++ testmodule.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 20 14:54:28 2017
+
+@author: root
+"""
+
+# Import module support
+import module
+
+# Now you can call defined function that module as follows
+module.print_func("Zara")
